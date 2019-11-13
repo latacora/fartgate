c85ec4963563c7335c1448f1f95dc52e587f611f
Patrick Farwick <pfarwick@latacora.com>
2019-11-13 13:57:33 -0600

    basic working test and pulling json

--- src/fartgate/core.clj
+++ src/fartgate/core.clj
@@ -1,7 +1,8 @@
 (ns fartgate.core
   (:require
    [cheshire.core :as json]
-   [taoensso.timbre :as log])
+   [taoensso.timbre :as log]
+   [clojure.java.io :as io])
   (:gen-class
    :implements [com.amazonaws.services.lambda.runtime.RequestStreamHandler]))
 
@@ -14,5 +15,6 @@
   "Given a Lambda S3 event, finds all object puts in the event and sends diffs off
   somewhere."
   [this in-stream out-stream context]
-  (let [parsed (-> in-stream parse-json log/spy)]
-    parsed))
+  (let [parsed (-> in-stream parse-json)
+        container {:container (get-in parsed [:detail :containers 0 :exitCode])}] ;;log/spy
+      (log/info container) container))
--- test/fartgate/core_test.clj
+++ test/fartgate/core_test.clj
@@ -1,5 +1,7 @@
 (ns fartgate.core-test
   (:require [clojure.test :as t]
-            [fartgate.core :as f]))
+            [fartgate.core :as f]
+            [cheshire.core :as json]))
 
-(t/deftest )
+(t/deftest default (t/is (= (f/-handleRequest nil (clojure.java.io/reader "dev-resources/test.json") nil nil) {:container 137})))
+
